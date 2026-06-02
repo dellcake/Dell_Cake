cb24595c7227bfb3fcbab831b08cfa9283be9331
dellcake <90687108+dellcake@users.noreply.github.com>
2026-06-02 19:56:30 +0330

    Update script.js

--- script.js
+++ script.js
@@ -3,43 +3,35 @@ const intro = document.getElementById("introBox");
 const buttons = document.getElementById("buttons");
 
 setTimeout(() => {
-
-logo.classList.remove("hidden");
-logo.classList.add("logoAnimate");
-
+  logo.classList.remove("hidden");
+  logo.classList.add("logoAnimate");
 }, 300);
 
 setTimeout(() => {
-
-intro.classList.remove("hidden");
-intro.classList.add("show");
-
+  intro.classList.remove("hidden");
+  intro.classList.add("show");
 }, 1400);
 
 setTimeout(() => {
-
-buttons.classList.remove("hidden");
-buttons.classList.add("show");
-
+  buttons.classList.remove("hidden");
+  buttons.classList.add("show");
 }, 2800);
-new Swiper(".promoSwiper", {
 
-loop:true,
+document.addEventListener("DOMContentLoaded", function () {
 
-grabCursor:true,
+  new Swiper(".promoSwiper", {
+    loop: true,
+    grabCursor: true,
 
-centeredSlides:true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
 
-spaceBetween:20,
-
-pagination:{
-el:".swiper-pagination",
-clickable:true,
-},
-
-autoplay:{
-delay:4500,
-disableOnInteraction:false,
-},
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    }
+  });
 
 });
