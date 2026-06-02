e9b7360ebe8bcdea87eeef83d5d4c497576ae2cf
dellcake <90687108+dellcake@users.noreply.github.com>
2026-06-02 20:43:30 +0330

    Update script.js

--- script.js
+++ script.js
@@ -27,15 +27,6 @@ grabCursor:true,
 
 loop:true,
 
-centeredSlides:true,
-
-cardsEffect:{
-slideShadows:false,
-rotate:true,
-perSlideRotate:4,
-perSlideOffset:10,
-},
-
 autoplay:{
 delay:3500,
 disableOnInteraction:false,
