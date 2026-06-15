1e781ee0b598df1b6e33eac9d9f1c819875c27bf
dellcake <90687108+dellcake@users.noreply.github.com>
2026-06-15 21:47:15 +0330

    Update pdf.js

--- js/pdf.js
+++ js/pdf.js
@@ -1,4 +1,40 @@
-async function createOrderPDF(orderData) {
+console.log("DellCake PDF Module Loaded 💗");
+
+async function createOrderPDF(data) {
+
+    const pdfDoc = await PDFLib.PDFDocument.create();
+
+    const page = pdfDoc.addPage([595, 842]);
+
+    page.drawText("DellCake Test PDF 💗", {
+        x: 180,
+        y: 750,
+        size: 20
+    });
+
+    const pdfBytes = await pdfDoc.save();
+
+    const blob = new Blob(
+        [pdfBytes],
+        { type: "application/pdf" }
+    );
+
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement("a");
+
+    a.href = url;
+
+    a.download = "dellcake-test.pdf";
+
+    a.click();
+
+    URL.revokeObjectURL(url);
 
 }
-console.log("DellCake PDF Module Loaded 💗");
+
+window.testPDF = () => {
+
+    createOrderPDF({});
+
+};
