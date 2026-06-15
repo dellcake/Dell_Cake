98be3c855a98938f3eea66354e17757c6bfefbc1
dellcake <90687108+dellcake@users.noreply.github.com>
2026-06-16 00:21:30 +0330

    Update script.js

--- script.js
+++ script.js
@@ -313,3 +313,53 @@ function shareOrder() {
     }
 
         }
+
+let currentMessage = "";
+
+/* باز کردن مودال */
+function openShareModal(message) {
+    currentMessage = message;
+
+    document.getElementById("shareModal").classList.remove("hidden");
+}
+
+/* بستن مودال */
+function closeShareModal() {
+    document.getElementById("shareModal").classList.add("hidden");
+}
+
+/* تلگرام */
+function openTelegram() {
+    const url = `https://t.me/share/url?text=${encodeURIComponent(currentMessage)}`;
+    window.open(url, "_blank");
+}
+
+/* بله */
+function openBale() {
+    const url = `https://ble.ir/dellcake_pv?text=${encodeURIComponent(currentMessage)}`;
+    window.open(url, "_blank");
+}
+
+/* پیامک */
+function openSMS() {
+    const phone = "09102768171";
+    const url = `sms:${phone}?body=${encodeURIComponent(currentMessage)}`;
+    window.location.href = url;
+}
+
+/* events */
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.getElementById("closeShare")
+        ?.addEventListener("click", closeShareModal);
+
+    document.getElementById("shareTelegram")
+        ?.addEventListener("click", openTelegram);
+
+    document.getElementById("shareBale")
+        ?.addEventListener("click", openBale);
+
+    document.getElementById("shareSMS")
+        ?.addEventListener("click", openSMS);
+
+});
