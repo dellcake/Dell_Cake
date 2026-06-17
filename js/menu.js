dd2208be69fb04e8e5ce427b20bfcfb84310788e
dellcake <90687108+dellcake@users.noreply.github.com>
2026-06-17 09:14:25 +0330

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    /* =========================
+       منوی کناری
+    ========================= */
+
+    const menuBtn = document.querySelector(".menu-btn");
+    const sideMenu = document.getElementById("sideMenu");
+    const overlay = document.getElementById("menuOverlay");
+
+    if (menuBtn && sideMenu && overlay) {
+
+        menuBtn.addEventListener("click", () => {
+            sideMenu.classList.toggle("active");
+            overlay.classList.toggle("active");
+        });
+
+        overlay.addEventListener("click", () => {
+            sideMenu.classList.remove("active");
+            overlay.classList.remove("active");
+        });
+
+    }
