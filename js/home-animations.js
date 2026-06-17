eb6171f8ea71d208b86ee9cc4f9def7dbd9fbd1b
dellcake <90687108+dellcake@users.noreply.github.com>
2026-06-17 06:29:46 +0000

    Update home-animations.js

--- js/home-animations.js
+++ js/home-animations.js
@@ -1,1 +1,36 @@
+/* =========================
+   انیمیشن صفحه اصلی
+========================= */
 
+const intro = document.getElementById("introBox");
+const contactText = document.getElementById("contactText");
+const buttons = document.getElementById("buttons");
+const gallery = document.getElementById("gallerySection");
+
+if (intro) {
+    setTimeout(() => {
+        intro.classList.add("show");
+        intro.classList.remove("hidden");
+    }, 500);
+}
+
+if (contactText) {
+    setTimeout(() => {
+        contactText.classList.add("show");
+        contactText.classList.remove("hidden");
+    }, 1000);
+}
+
+if (buttons) {
+    setTimeout(() => {
+        buttons.classList.add("show");
+        buttons.classList.remove("hidden");
+    }, 1500);
+}
+
+if (gallery) {
+    setTimeout(() => {
+        gallery.classList.add("show");
+        gallery.classList.remove("hidden");
+    }, 2000);
+}
