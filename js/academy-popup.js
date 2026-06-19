0b737f2b282a49516d0228ed327e0435e24ea24b
dellcake <90687108+dellcake@users.noreply.github.com>
2026-06-19 21:16:58 +0330

    Create academy-popup.js

---
+++ js/academy-popup.js
@@ -0,0 +1,7 @@
+function openAcademyPopup() {
+    document.getElementById("academyPopup").style.display = "flex";
+}
+
+function closeAcademyPopup() {
+    document.getElementById("academyPopup").style.display = "none";
+}
