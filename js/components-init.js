2314d1140c26168abe16808c45f9aca5553f20ea
dellcake <90687108+dellcake@users.noreply.github.com>
2026-06-20 03:42:57 -0700

    Create components-init.js

---
+++ js/components-init.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    loadComponent(
+        "header-container",
+        "components/header.html"
+    );
+
+    loadComponent(
+        "side-menu-container",
+        "components/side-menu.html"
+    );
+
+    loadComponent(
+        "footer-container",
+        "components/footer.html"
+    );
+
+});
