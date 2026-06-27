a6b63d96c4bbc94e299f622ebad17ef1c9ffbfc5
dellcake <90687108+dellcake@users.noreply.github.com>
2026-06-27 22:03:00 +0330

    Update load-components.js

--- admin/js/load-components.js
+++ admin/js/load-components.js
@@ -1,1 +1,59 @@
+/* ==========================================
+        Dell Cake CMS
+        Component Loader
+========================================== */
 
+document.addEventListener("DOMContentLoaded", async () => {
+
+    await loadComponent(
+        "#sidebarContainer",
+        "components/sidebar.html"
+    );
+
+    await loadComponent(
+        "#headerContainer",
+        "components/header.html"
+    );
+
+    initSidebar();
+
+});
+
+
+/* ==========================================
+        Load HTML Component
+========================================== */
+
+async function loadComponent(
+    selector,
+    file
+){
+
+    const container =
+        document.querySelector(selector);
+
+    if(!container) return;
+
+    try{
+
+        const response =
+            await fetch(file);
+
+        const html =
+            await response.text();
+
+        container.innerHTML =
+            html;
+
+    }
+
+    catch(error){
+
+        console.error(
+            "Component Error:",
+            error
+        );
+
+    }
+
+}
