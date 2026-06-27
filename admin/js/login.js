43d5671554bcf9e334f4226338a821a56c7a373c
dellcake <90687108+dellcake@users.noreply.github.com>
2026-06-27 05:28:22 +0330

    Update login.js

--- admin/js/login.js
+++ admin/js/login.js
@@ -1,1 +1,55 @@
+import {
 
+auth,
+provider
+
+}
+
+from "../../js/firebase-auth.js";
+
+import {
+
+signInWithPopup
+
+}
+
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+const adminEmail =
+"ایمیل-خودت@gmail.com";
+
+document
+.getElementById("googleLogin")
+.addEventListener("click", async () => {
+
+try{
+
+const result =
+await signInWithPopup(auth,provider);
+
+const user =
+result.user;
+
+if(user.email===adminEmail){
+
+location.href="dashboard.html";
+
+}else{
+
+alert("شما مدیر سایت نیستید.");
+
+await auth.signOut();
+
+location.href="../index.html";
+
+}
+
+}
+
+catch(error){
+
+console.log(error);
+
+}
+
+});
