bb684fc07e6898876adafef65bf35bdc6c16ab30
dellcake <90687108+dellcake@users.noreply.github.com>
2026-06-27 05:11:58 +0330

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCl59WACCFGKiT3Jze951TWu53nWrDoLyw",
+  authDomain: "dell-cake.firebaseapp.com",
+  databaseURL: "https://dell-cake-default-rtdb.firebaseio.com",
+  projectId: "dell-cake",
+  storageBucket: "dell-cake.firebasestorage.app",
+  messagingSenderId: "776777557142",
+  appId: "1:776777557142:web:e28e3031ce967116054eb7"
+};
