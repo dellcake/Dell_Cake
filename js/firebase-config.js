6ca953c2b1cea5cdecd86df49084be4c227a0e40
dellcake <90687108+dellcake@users.noreply.github.com>
2026-06-27 05:18:28 +0330

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,18 +1,23 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCl59WACCFGKiT3Jze951TWu53nWrDoLyw",
-  authDomain: "dell-cake.firebaseapp.com",
-  databaseURL: "https://dell-cake-default-rtdb.firebaseio.com",
-  projectId: "dell-cake",
-  storageBucket: "dell-cake.firebasestorage.app",
-  messagingSenderId: "776777557142",
-  appId: "1:776777557142:web:e28e3031ce967116054eb7"
+
+    apiKey: "AIzaSyCl59WACCFGKiT3Jze951TWu53nWrDoLyw",
+
+    authDomain: "dell-cake.firebaseapp.com",
+
+    databaseURL: "https://dell-cake-default-rtdb.firebaseio.com",
+
+    projectId: "dell-cake",
+
+    storageBucket: "dell-cake.firebasestorage.app",
+
+    messagingSenderId: "776777557142",
+
+    appId: "1:776777557142:web:e28e3031ce967116054eb7"
+
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export { app };
