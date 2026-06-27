4f3abaf726587378649c26208a78dfd88dd0d775
dellcake <90687108+dellcake@users.noreply.github.com>
2026-06-27 05:51:26 +0330

    Update firebase-auth.js

--- js/firebase-auth.js
+++ js/firebase-auth.js
@@ -1,11 +1,15 @@
 import { app } from "./firebase-config.js";
 
 import {
-
-getAuth
-
+    getAuth,
+    GoogleAuthProvider
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
 const auth = getAuth(app);
 
-export { auth };
+const provider = new GoogleAuthProvider();
+
+export {
+    auth,
+    provider
+};
